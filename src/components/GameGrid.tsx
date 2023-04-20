4b7c30fddf577c1b4c075d36c10e415df81d3572
OH <olegenikus@gmail.com>
2023-04-20 19:23:57 +0200

    games hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,47 +1,9 @@
-import React, { Fragment, useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+import React, { Fragment } from "react";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  slug: string;
-  name: string;
-  released: string;
-  tba: true;
-  background_image: string;
-  rating: number;
-  rating_top: number;
-  ratings: {};
-  ratings_count: number;
-  reviews_text_count: string;
-  added: number;
-  added_by_status: {};
-  metacritic: number;
-  playtime: number;
-  suggestions_count: number;
-  updated: string;
-  esrb_rating: {};
-  platforms: [];
-}
-
-interface FetchGamesResponse {
-  count: number;
-  next: string;
-  previous: string;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 function GameGrid() {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
-
+  const { games, error } = useGames();
   return (
     <Fragment>
       {error && <Text>{error}</Text>}

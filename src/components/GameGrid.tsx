f7238d6bb30883fc99b047afd46162de4fcec78c
OH <olegenikus@gmail.com>
2023-04-24 22:04:15 +0200

    game carts

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,17 +1,22 @@
 import React, { Fragment } from "react";
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 function GameGrid() {
   const { games, error } = useGames();
   return (
     <Fragment>
       {error && <Text>{error}</Text>}
-      <ul>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding={3}
+        spacing={10}
+      >
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard game={game} key={game.id}></GameCard>
         ))}
-      </ul>
+      </SimpleGrid>
     </Fragment>
   );
 }

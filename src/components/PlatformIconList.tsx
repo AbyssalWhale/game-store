0c23a297bb3fd1d2c9e47cf01931754f77f0472b
OH <olegenikus@gmail.com>
2023-04-24 23:07:13 +0200

    adding icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,19 +1,46 @@
 import React, { Fragment } from "react";
 import { Platform } from "../hooks/useGames";
-import { Text } from "@chakra-ui/react";
-import {} from "react-icons/fa";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
 
 interface Props {
   platforms: Platform[];
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation5: FaPlaystation,
+    playstation4: FaPlaystation,
+    playstation3: FaPlaystation,
+    "xbox-series-x": FaXbox,
+    "xbox-one": FaXbox,
+    xbox360: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
   return (
-    <Fragment>
+    <HStack marginY={"10px"}>
       {platforms.map((p) => (
-        <Text>{p.name}</Text>
+        <Icon as={iconMap[p.slug]} color="gray.500" />
       ))}
-    </Fragment>
+    </HStack>
   );
 };
 
